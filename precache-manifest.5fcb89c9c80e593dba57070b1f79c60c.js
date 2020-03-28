9ba85297d9d98c8530f0524e90f5dd54180ef4b8
gokulrajan12 <gokulrajan12@gmail.com>
2020-03-28 21:35:37 +0100

    Updates

---
+++ precache-manifest.5fcb89c9c80e593dba57070b1f79c60c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7a059344ad3b328bd65f58f9afadbb",
+    "url": "/techcorazon/index.html"
+  },
+  {
+    "revision": "ecf84735727502557f34",
+    "url": "/techcorazon/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0df49d918daec3ed033c",
+    "url": "/techcorazon/static/js/2.e80806cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/techcorazon/static/js/2.e80806cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf84735727502557f34",
+    "url": "/techcorazon/static/js/main.9fe5f55b.chunk.js"
+  },
+  {
+    "revision": "1a01a70954d7f5741176",
+    "url": "/techcorazon/static/js/runtime-main.8ac02230.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/techcorazon/static/media/logo.5d5d9eef.svg"
+  }
+]);
